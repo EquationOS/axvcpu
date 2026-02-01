529f80efe437354e644a76d0e7fcdadca5a27a39
hky1999 <976929993@qq.com>
2026-02-01 15:09:00 +0800

    [feat] add set_eptp_list_region

--- src/arch_vcpu.rs
+++ src/arch_vcpu.rs
@@ -94,5 +94,7 @@ pub trait AxVcpuAccessGuestState {
     fn set_ept_pointer(&mut self, eptp: EPTPointer) -> AxResult;
     fn eptp_list_region(&self) -> HostPhysAddr;
 
+    fn set_eptp_list_region(&mut self, addr: HostPhysAddr) -> AxResult;
+
     fn dump(&self);
 }
